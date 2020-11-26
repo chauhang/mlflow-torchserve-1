adb726ed6128c3132acc1fc33e95e99a15209d0b
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-27 03:01:12 +0530

    Fixing mnist example unit test
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- tests/examples/test_end_to_end_example.py
+++ tests/examples/test_end_to_end_example.py
@@ -6,20 +6,24 @@ from mlflow.utils import process
 @pytest.mark.usefixtures("start_torchserve")
 def test_mnist_example():
     os.environ["MKL_THREADING_LAYER"] = "GNU"
+    home_dir = os.getcwd()
     mnist_dir = "examples/MNIST"
     example_command = ["python", "mnist_model.py", "--max_epochs", "1"]
-    process.exec_cmd(example_command, cwd=mnist_dir, shell=True)
+    process.exec_cmd(example_command, cwd=mnist_dir)
+
+    assert os.path.exists(os.path.join(mnist_dir, "model.pth"))
     create_deployment_command = [
         "python",
         "create_deployment.py",
-        "--deployment_name",
-        "mnist_test",
         "--export_path",
-        os.path.join(os.getcwd(), "model_store"),
+        os.path.join(home_dir, "model_store"),
     ]
-    process.exec_cmd(create_deployment_command, cwd=mnist_dir, shell=True)
 
-    predict_command = ["python", "predict.py", "--deployment_name", "mnist_test"]
+    process.exec_cmd(create_deployment_command, cwd=mnist_dir)
+
+    assert os.path.exists(os.path.join(home_dir, "model_store", "mnist_classification.mar"))
+
+    predict_command = ["python", "predict.py"]
     res = process.exec_cmd(predict_command, cwd=mnist_dir)
     assert "ONE" in res[1]
 
